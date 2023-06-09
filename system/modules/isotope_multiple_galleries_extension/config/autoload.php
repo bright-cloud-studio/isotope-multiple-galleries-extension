458020fb88471a0d2c65b69583755c26c9f084df
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-09 12:38:32 -0400

    Create autoload.php

---
+++ system/modules/isotope_multiple_galleries_extension/config/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+
+ClassLoader::addClasses(array
+(
+	'IsotopeStandardExtended\Model\Product' 		=> 'system/modules/isotope_multiple_galleries_extension/library/Bcs/Model/Product/StandardExtended.php'
+));
